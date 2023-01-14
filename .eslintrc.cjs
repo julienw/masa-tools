f55b98bd7fccbee27d699860e2a2ba330505a8f5
Julien Wajsberg <felash@gmail.com>
2023-01-14 18:00:13 +0100

    Add eslint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: [
+    "preact",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    // We don't use Jest in this project, so without disabling this rule, eslint throws.
+    "jest/no-deprecated-functions": 0,
+  },
+};
