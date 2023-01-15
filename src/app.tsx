f60cdb5a3b70e5d9aa7b3c2528e4b816a0f1ed35
Julien Wajsberg <felash@gmail.com>
2023-01-15 22:31:00 +0100

    Remove the baseurl that doesn't work with the history module

--- src/app.tsx
+++ src/app.tsx
@@ -6,19 +6,15 @@ import Export from "./pages/export";
 import NotFound from "./pages/_404";
 
 export function App() {
-  let baseUrl = import.meta.env.BASE_URL;
-  if (baseUrl.endsWith("/")) {
-    baseUrl = baseUrl.slice(0, -1);
-  }
   return (
     <>
       <Header />
         {/* eslint-disable-line */ /* @ts-ignore type we know this works, the type is just incorrect*/}
       <Router history={createHashHistory()}>
         {/* eslint-disable-line */ /* @ts-ignore type {path:string} is not assignalbe to type IntrinsicAttributes*/}
-        <Home path={`${baseUrl}/`} />
+        <Home path="/" />
         {/* eslint-disable-line */ /* @ts-ignore type {path:string} is not assignalbe to type IntrinsicAttributes*/}
-        <Export path={`${baseUrl}/export`} />
+        <Export path="/export" />
         {/* eslint-disable-line */ /* @ts-ignore type {default} is not assignalbe to type IntrinsicAttributes*/}
         <NotFound default />
       </Router>
--- src/header.tsx
+++ src/header.tsx
@@ -1,14 +1,10 @@
 import { Link } from "preact-router/match";
 export default function Header() {
-  let baseUrl = import.meta.env.BASE_URL;
-  if (baseUrl.endsWith("/")) {
-    baseUrl = baseUrl.slice(0, -1);
-  }
   return (
     <header>
       <nav>
-        <Link href={`${baseUrl}/`}>Home</Link>
-        <Link href={`${baseUrl}/export`}>Export</Link>
+        <Link href="/">Home</Link>
+        <Link href="/export">Export</Link>
       </nav>
     </header>
   );
