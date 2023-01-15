802f3fb4ef13a3338cef20743fae7b400b209349
Julien Wajsberg <felash@gmail.com>
2023-01-15 22:15:19 +0100

    Add the default route

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import Router from "preact-router";
 import Header from "./header";
 import Home from "./pages/home";
 import Export from "./pages/export";
+import NotFound from "./pages/_404";
 
 export function App() {
   let baseUrl = import.meta.env.BASE_URL;
@@ -16,6 +17,7 @@ export function App() {
         <Home path={`${baseUrl}/`} />
         {/* eslint-disable-line */ /* @ts-ignore type {path:string} is not assignalbe to type IntrinsicAttributes*/}
         <Export path={`${baseUrl}/export`} />
+        <NotFound default />
       </Router>
     </>
   );
