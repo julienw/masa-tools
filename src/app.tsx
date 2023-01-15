b2ec3e9e9e936c3f6cf9851ba0c1e62a2bf1b2cf
Julien Wajsberg <felash@gmail.com>
2023-01-15 22:27:25 +0100

    Use the npm module history

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import Router from "preact-router";
+import { createHashHistory } from "history";
 import Header from "./header";
 import Home from "./pages/home";
 import Export from "./pages/export";
@@ -12,11 +13,13 @@ export function App() {
   return (
     <>
       <Header />
-      <Router>
+        {/* eslint-disable-line */ /* @ts-ignore type we know this works, the type is just incorrect*/}
+      <Router history={createHashHistory()}>
         {/* eslint-disable-line */ /* @ts-ignore type {path:string} is not assignalbe to type IntrinsicAttributes*/}
         <Home path={`${baseUrl}/`} />
         {/* eslint-disable-line */ /* @ts-ignore type {path:string} is not assignalbe to type IntrinsicAttributes*/}
         <Export path={`${baseUrl}/export`} />
+        {/* eslint-disable-line */ /* @ts-ignore type {default} is not assignalbe to type IntrinsicAttributes*/}
         <NotFound default />
       </Router>
     </>
